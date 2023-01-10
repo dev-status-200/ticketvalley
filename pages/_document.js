8ce4310384f3ac25a31fda6385e5651a7020d20a
DESKTOP-GN1I4I1\HPP <syedabdullahteamhail@gmail.com>
2023-01-10 19:11:45 +0500

    update

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MainDocument extends Document {
         <link rel="icon" href="/images/logo.png" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" as="font" crossOrigin="" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin={true} />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
 
       </Head>
