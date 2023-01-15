bd7c423fb3b1bf689ab55c5748d7f75d8282af7a
dev-status-200 <dev.status.200@gmail.com>
2023-01-16 01:15:47 +0500

    update

---
+++ Components/Layouts/Transport/index.js
@@ -0,0 +1,108 @@
+import { Row, Col, Table } from 'react-bootstrap';
+import React, { useEffect, useReducer } from 'react';
+import Router from 'next/router';
+import { Modal } from 'antd';
+import CreateOrEdit from './CreateOrEdit';
+import { EditOutlined, HistoryOutlined } from '@ant-design/icons';
+
+function recordsReducer(state, action){
+    switch (action.type) {
+      case 'toggle': { 
+        return { ...state, [action.fieldName]: action.payload } 
+      }
+      case 'create': {
+        return {
+            ...state,
+            edit: false,
+            visible: true,
+        }
+      }
+      case 'edit': {
+        return {
+            ...state,
+            selectedRecord:{},
+            edit: true,
+            visible: true,
+            selectedRecord:action.payload
+        }
+      }
+      case 'modalOff': {
+        let returnVal = { ...state, visible: false, edit: false };
+        state.edit?returnVal.selectedRecord={}:null
+        return returnVal
+      }
+      default: return state 
+    }
+}
+
+const baseValues = {
+  //Basic Info
+  id:'',
+  name:"",
+  price:"",
+  status:"1"
+}
+
+const initialState = {
+    records: [],
+    load:false,
+    visible:false,
+    edit:false,
+    values:baseValues,
+    // Editing Records
+    selectedRecord:{},
+    oldRecord:{},
+};
+
+const Transport = ({transportData}) => {
+    const [ state, dispatch ] = useReducer(recordsReducer, initialState);
+    useEffect(() => {
+        dispatch({type:"toggle", fieldName:"records", payload:transportData.result})
+    }, [])
+    
+  return (
+    <div>
+    <Row>
+        <Col><h5>Transport</h5></Col>
+        <Col><button className='btn-custom right' onClick={()=>dispatch({type:'create'})}>Create</button></Col>
+    </Row>
+    <Row style={{maxHeight:'69vh',overflowY:'auto', overflowX:'hidden'}}>
+    <Col md={12}>
+      <div className='table-sm-1 mt-3' style={{maxHeight:500, overflowY:'auto'}}>
+        <Table className='tableFixHead'>
+        <thead>
+          <tr>
+            <th>Sr.</th>
+            <th>Name</th>
+            <th>Price
+                
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+        {state.records.map((x, index) => {
+          return (
+          <tr key={index} className='f'>
+            <td> {x.name} </td>
+            <td> {x.price} </td>
+            <td> <span> <EditOutlined className='modify-edit' onClick={()=>dispatch({type:'edit', payload:x})}/> </span> </td>
+          </tr>
+          )
+        })}
+        </tbody>
+        </Table>
+      </div>
+    </Col>
+    </Row>
+    <Modal
+      open={state.visible}
+      onOk={()=>dispatch({ type: 'modalOff' })} onCancel={()=>dispatch({ type: 'modalOff' })}
+      width={450} footer={false} centered={false}
+    >
+      <CreateOrEdit state={state} dispatch={dispatch} baseValues={baseValues} />
+    </Modal>
+    </div>
+  )
+}
+
+export default Transport
