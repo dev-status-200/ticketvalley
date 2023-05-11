b4bb67eade44e397f0b19aca31d9d18239580716
DESKTOP-GN1I4I1\HPP <syedabdullahteamhail@gmail.com>
2023-05-11 19:04:30 +0500

    update

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,10 @@ class MainDocument extends Document {
   render() {
     return (
     <Html>
-      {/* <title>Tickets Valley | Travel & Tours</title> */}
+      <title>Tickets Valley | Travel & Tours</title>
       <Head >
         <meta name="description" content="Tickets Valley" />
-        {/* <link rel="icon" href="/images/logo.png" /> */}
+        <link rel="icon" href="/images/logo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" as="font" crossOrigin="" />
         <link rel="preconnect" href="https://fonts.gstatic.com"  />
         <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet"></link>
