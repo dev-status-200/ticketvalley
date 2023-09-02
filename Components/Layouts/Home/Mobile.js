2775cca34e60da51bfa13631471f91f2095fbbae
dev-status-200 <dev.status.200@gmail.com>
2023-09-02 12:32:32 +0500

    update

--- Components/Layouts/Home/Mobile.js
+++ Components/Layouts/Home/Mobile.js
@@ -5,10 +5,17 @@ import { Row, Col, Container } from 'react-bootstrap';
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Autoplay, Navigation } from "swiper";
+import { Navigation } from "swiper";
 import "swiper/css/bundle";
 import MobileCard from '../../Shared/MobileCard';
 import SignUp from '../../Shared/SignUp';
+import dynamic from 'next/dynamic';
+const Adventures = dynamic(() => import('./AdventureTours'), {
+  loading: () => <div className='text-center'> <img src='/loader.svg' /> </div>,
+})
+const Combos = dynamic(() => import('./Combos'), {
+  loading: () => <div className='text-center'> <img src='/loader.svg' /> </div>,
+})
 
 const Mobile = ({combos, adventures, bestSelling}) => {
   return (
@@ -21,45 +28,12 @@ const Mobile = ({combos, adventures, bestSelling}) => {
     </div>
     <div className=' bg-02' style={{backgroundColor:"white"}}>
       <Container className='my-5' data-aos='fade-up'>
-          <h3 className='mt-3 fw-700 text-center'>ADVENTURES & TOUR<span className='blue-txt'> ACTIVITIES</span></h3>
-          <Swiper slidesPerView={2} spaceBetween={10} 
-              modules={[Navigation]}
-              navigation={true}
-              className="mySwiper"
-              autoplay={{
-                  delay: 2500,
-                  disableOnInteraction: false,
-              }}
-          >
-              {adventures?.map((x, i)=>{
-                return(
-                  <SwiperSlide className='' key={i}>
-                      <MobileCard tour={x} height={110} info={true} font={12  } />
-                  </SwiperSlide>
-                )})}
-          </Swiper>
-          {adventures?.length==0 && <div className='text-center'> <img src='/loader.svg' /> </div>}
+        <h3 className='mt-3 fw-700 text-center'>ADVENTURES & TOUR<span className='blue-txt'> ACTIVITIES</span></h3>
+        <Adventures/>
       </Container>
       <Container className='my-0 pb-5' data-aos='fade-up'>
-          <h3 className=' fw-700 text-center'>COMBO TOUR<span className='blue-txt'>  ACTIVITIES</span></h3>
-          <Swiper slidesPerView={2} spaceBetween={10} 
-              modules={[Navigation]}
-              navigation={true}
-              className="mySwiper"
-              autoplay={{
-                  delay: 2500,
-                  disableOnInteraction: false,
-              }}
-          >
-              {combos.map((x, i)=>{
-                  return(
-                    <SwiperSlide className='' key={i}>
-                        <MobileCard tour={x} height={110} info={true} font={12  } />
-                    </SwiperSlide>
-                  )
-              })}
-          </Swiper>
-          {combos?.length==0 && <div className='text-center'> <img src='/loader.svg' /> </div>}
+        <h3 className=' fw-700 text-center'>COMBO TOUR<span className='blue-txt'>  ACTIVITIES</span></h3>
+        <Combos/>
       </Container>
       <SignUp mobile={true} />
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from '/Components/Layouts/Home';
+import axios from 'axios';
 
 const index = () => {
   return (
